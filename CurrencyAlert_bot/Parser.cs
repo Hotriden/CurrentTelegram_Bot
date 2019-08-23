aa808edbd6c17a21fe9e4016dca85f6e74c74e57
Hotriden <bloodridetv@gmail.com>
2019-08-23 17:50:04 +0300

    some shit on work

--- CurrencyAlert_bot/Parser.cs
+++ CurrencyAlert_bot/Parser.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using AngleSharp.Html.Parser;
+using HtmlAgilityPack;
 
 namespace CurrencyAlert_bot
 {
     public class Parser
     {
+        /// <summary>
+        /// Minfin
+        /// </summary>
+        /// <param name="ClassToGet"></param>
+        #region Minfin
+        HtmlWeb parse = new HtmlWeb();
+        private HttpClient httpClient;
 
+        public void GetCurrentMinfin(string ClassToGet)
+        {
+            HtmlDocument document = parse.Load("https://minfin.com.ua/currency/mb/");
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//td[@class='" + ClassToGet + "']"))
+            {
+                string value = node.InnerText;
+                Console.WriteLine("String - " + value);
+            }
+        }
+        #endregion
+
+        #region Obmenka
+        public async Task GetCurrentObmenka()
+        {
+            string responseBodyAsText;
+            string status = "Waiting for response ...";
+
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+
+            HttpResponseMessage response = await httpClient.GetAsync("https://obmenka.kharkov.ua/usd-uah");
+            response.EnsureSuccessStatusCode();
+
+            status = response.StatusCode + " " + response.ReasonPhrase + Environment.NewLine;
+            responseBodyAsText = await response.Content.ReadAsStringAsync();
+
+            //HttpWebRequest proxy_request = (HttpWebRequest)WebRequest.Create("https://obmenka.kharkov.ua/usd-uah");
+
+            #endregion
+        }
+
+        #region Goverla
+        public void GetCurrentGoverla(string ClassToGet)
+        {
+            HtmlDocument document = parse.Load("https://goverla.ua/");
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//div[@class='" + ClassToGet + "']"))
+            {
+                string value = node.InnerText;
+                Console.WriteLine("String - " + value);
+            }
+        }
+        #endregion
     }
 }
--- CurrencyAlert_bot/Start.cs
+++ CurrencyAlert_bot/Start.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using CurrencyAlert_bot.Entities;
-using AngleSharp;
-using AngleSharp.Html.Parser;
 using System.Net;
 using System.IO;
 
@@ -16,23 +14,23 @@ namespace CurrencyAlert_bot
     public class Start
     {
         private static readonly TelegramBotClient Bot = new TelegramBotClient("987528104:AAGz3x4AggzIoG6pj7aYkXNcf23q2J56sm8");
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            client.Headers.Add("user-agent", "Mozilla/5.0 (Windows; Windows NT 5.1; rv:1.9.2.4) Gecko/20100611 Firefox/3.6.4");
-            string fullPage = client.DownloadString("https://minfin.com.ua/ua/currency/mb/");
-            string[] some = fullPage.Split(new string[] { "<td data-title='Доллар' class='active'>" }, StringSplitOptions.None);
-            foreach(var b in some)
-            {
-                Console.WriteLine(b);
-            }
-
-
+            Parser parser = new Parser();
+            parser.GetCurrentGoverla("gvrl-table-cell bid");
+            parser.GetCurrentMinfin("active");
+            CheckObmenka();
             Bot.OnMessage += Bot_OnMessage;
             Bot.StartReceiving();
             Bot.SendTextMessageAsync("@CurrencyAlert_Bot", "show");
             Console.ReadKey();
             Bot.StopReceiving();
+        }
+
+        static async void CheckObmenka()
+        {
+            Parser parser = new Parser();
+            await parser.GetCurrentObmenka();
         }
 
         private static void Bot_OnMessage(object sender, MessageEventArgs e)
